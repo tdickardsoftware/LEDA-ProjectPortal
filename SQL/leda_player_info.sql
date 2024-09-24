d928a1fd52cc2597ff87da48c49f589c5ab86f13
Tyler Dickard <tdickardsoftware@gmail.com>
2024-09-23 23:09:21 -0400

    Create place info table

--- SQL/leda_player_info.sql
+++ SQL/leda_player_info.sql
@@ -4,20 +4,20 @@
 
 CREATE TABLE IF NOT EXISTS public.leda_player_info
 (
-    id bigint NOT NULL,
+    "id" bigint NOT NULL,
     "ledaId" bigint,
     "lastName" text COLLATE pg_catalog."default" NOT NULL,
     "firstName" text COLLATE pg_catalog."default" NOT NULL,
     "middleInitial" character varying(1) COLLATE pg_catalog."default" NOT NULL,
     "addressOne" text COLLATE pg_catalog."default" NOT NULL,
     "addressTwo" text COLLATE pg_catalog."default",
-    city text COLLATE pg_catalog."default" NOT NULL,
-    state text COLLATE pg_catalog."default" NOT NULL,
-    zip character varying(9) COLLATE pg_catalog."default" NOT NULL,
+    "city" text COLLATE pg_catalog."default" NOT NULL,
+    "state" text COLLATE pg_catalog."default" NOT NULL,
+    "zip" character varying(9) COLLATE pg_catalog."default" NOT NULL,
     "phoneNumber" text COLLATE pg_catalog."default" NOT NULL,
     "otherNumber" text COLLATE pg_catalog."default",
-    email text COLLATE pg_catalog."default" NOT NULL,
-    gender text COLLATE pg_catalog."default" NOT NULL,
+    "email" text COLLATE pg_catalog."default" NOT NULL,
+    "gender" text COLLATE pg_catalog."default" NOT NULL,
     "dateOfBirth" date NOT NULL,
     CONSTRAINT leda_player_info_pkey PRIMARY KEY (id)
 )
