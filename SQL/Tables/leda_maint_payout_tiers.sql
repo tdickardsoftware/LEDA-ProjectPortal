3c9110f47bf1bb9d076a79381d1923145b4ee9fd
Tyler Dickard <tdickardsoftware@gmail.com>
2024-09-24 15:14:56 -0400

    Add leda_maint_payout_tiers

--- SQL/Tables/leda_maint_payout_tiers.sql
+++ SQL/Tables/leda_maint_payout_tiers.sql
@@ -5,8 +5,8 @@
 CREATE TABLE IF NOT EXISTS maint.leda_maint_payout_tiers
 (
     id bigint NOT NULL,
-    place bigint NOT NULL,
-    amount numeric(12,2) NOT NULL,
+    "place" bigint NOT NULL,
+    "amount" numeric(12,2) NOT NULL,
     CONSTRAINT leda_maint_payout_tiers_pkey PRIMARY KEY (id)
 )
 
