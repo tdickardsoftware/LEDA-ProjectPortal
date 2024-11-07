8ee0601b3b073997d272f909af5dd992a07805c2
Tyler Dickard <tdickardsoftware@gmail.com>
2024-11-06 21:41:58 -0500

    Move player type to definitions file

--- leda_project_portal/schemas/managment/players.tsx
+++ leda_project_portal/schemas/managment/players.tsx
@@ -6,26 +6,7 @@
 // Imports
 //
 import { ColumnDef } from "@tanstack/react-table"
-//
-//Define type to identify the shape of our data coming from postgres
-//
-export type Player = {
-    id: number
-    ledaId: number
-    lastName: string
-    firstName: string
-    middleInitial: string
-    addressOne: string
-    addressTwo: string
-    city: string
-    state: string
-    zip: string
-    phoneNumber: string
-    otherNumber: string
-    email: string
-    gender: string
-    dateOfBirth: Date
-}
+import { Player } from "@/lib/definitions"
 //
 //Define the columns
 //
