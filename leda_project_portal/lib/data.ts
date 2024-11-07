2e298d86ea6713865cb8b2874ec329ff771f6c4c
Tyler Dickard <tdickardsoftware@gmail.com>
2024-11-06 21:55:42 -0500

    Add comments

--- leda_project_portal/lib/data.ts
+++ leda_project_portal/lib/data.ts
@@ -1,10 +1,17 @@
+//
+// Imports
+//
 import { sql } from '@vercel/postgres';
 import {Player, } from './definitions'
-
+//
+// async function to get all player data from the database
+//
 export async function fetchPlayers() {
+    // attempt to get data
     try {
         const data = await sql<Player>`SELECT * from public.leda_player_info`;
         return data.rows;
+    // if it cannot get data error out
     } catch(error) {
         console.error('Database Error: ', error)
         throw new Error('Failed to fetch Player Information')
