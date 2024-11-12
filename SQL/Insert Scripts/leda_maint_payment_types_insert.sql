26f5f7383e96c9598561b487825e8ce25750072f
Tyler Dickard <tdickardsoftware@gmail.com>
2024-11-11 23:37:52 -0500

    update tables to use sequence and add data insert scripts

---
+++ SQL/Insert Scripts/leda_maint_payment_types_insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO maint.leda_maint_payment_types(
+	"paymentType", "desc")
+	VALUES ('Banq','Banquet'),
+			('Bar','Bar Fee'),
+			('Late','Late Scoresheet'),
+			('Memb','Membership'),
+			('Other','Miscellaneous'),
+			('Part','Partial Members'),
+			('PICNC','Summer Picnic'),
+			('Steam','Summer Team Fee'),
+			('Team','Team Fee'),
+			('Week','Weekly Fee')
