210eb530d342c619278ff266f91dd5b6d4ea0fd2
Tyler Dickard <tdickardsoftware@gmail.com>
2024-12-19 23:48:36 -0500

    Define form schema

--- leda_project_portal/components/place-add-form.tsx
+++ leda_project_portal/components/place-add-form.tsx
@@ -28,5 +28,24 @@ import SeasonCodeSelector from './ui/season-code-selector';
 
 const placeFormSchema = z.object({ 
     ledaId: z.number().min(0, { message: 'LEDA ID Must be a Postive Number.' }),
-
+    name: z.string().min(1, { message: 'Name is required.' }),
+    addressOne: z.string().min(1, { message: 'Address is required.' }),
+    addressTwo: z.string().optional(),
+    city: z.string().min(1, { message: 'City is required.' }),
+    state: z.string().min(1, { message: 'State is required.' }),
+    zip: z.string().min(1, { message: 'Zip is required.' }),
+    phoneNumber: z.string().min(1, { message: 'Phone Number is Required' }).refine((value) => isValidPhoneNumber(value, 'US'), { message: 'Phone Number is Invalid' }),
+    otherNumber: z.string().optional().refine((value) => value === '' ||  isValidPhoneNumber(value ?? '', 'US'), { message: 'Other Number is Invalid' }),
+    email: z.string().min(1, { message: 'Email is Required' }).refine(validator.isEmail, { message: 'Email is Invalid' }),
+    website: z.string().optional().refine((value) => value === undefined || validator.isURL(value), { message: 'Website is Invalid' }),
+    establishedDate: z.string(),
+    memo: z.string().optional(),
+    numberOfBoards: z.number().min(0, { message: 'Number of Boards Must be a Postive Number.' }),
+    sendMailings: z.boolean(),
+    regularSponsor: z.boolean(),
+    currentSponsor: z.boolean(),
+    issues: z.boolean(),
+    lastBarFeePayment: z.string(),
+    lastSanctioningDate: z.string().optional(),
+    placeType: z.string().min(1, { message: 'Place Type is Required' }),
 });
