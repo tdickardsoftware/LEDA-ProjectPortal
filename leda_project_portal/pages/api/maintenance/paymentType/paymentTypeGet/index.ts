24443042af610307040b17d81b1a61901681d48c
Tyler Dickard <tdickardsoftware@gmail.com>
2024-12-27 22:56:57 -0500

    Update query to select from the correct table

--- leda_project_portal/pages/api/maintenance/paymentType/paymentTypeGet/index.ts
+++ leda_project_portal/pages/api/maintenance/paymentType/paymentTypeGet/index.ts
@@ -7,7 +7,7 @@ import { PaymentType} from '@/lib/definitions';
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'GET') {
     try {
-      const result = await query<PaymentType>('SELECT "paymentType", "desc" FROM maint.leda_maint_divisions;');
+      const result = await query<PaymentType>('SELECT "paymentType", "desc" FROM maint.leda_maint_payment_types;');
       res.status(200).json(result.rows);
     } catch (error) {
       res.status(500).json({ error: 'Failed to fetch payment types' });
