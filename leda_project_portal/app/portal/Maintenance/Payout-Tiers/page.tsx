657dc54516bd4525828b41bb8fa5e100ce061946
Tyler Dickard <tdickardsoftware@gmail.com>
2024-12-27 23:15:13 -0500

    Fix DataTable to render correctly

--- leda_project_portal/app/portal/Maintenance/Payout-Tiers/page.tsx
+++ leda_project_portal/app/portal/Maintenance/Payout-Tiers/page.tsx
@@ -1,4 +1,5 @@
 import { DataTable } from "@/components/datatable";
+import { DialogWithButton } from "@/components/dialog-button";
 import { fetchPayoutTiers } from "@/lib/getData";
 import { columns } from "@/schemas/maintenance/payout_tiers";
 import { Metadata } from "next";
@@ -11,7 +12,7 @@ export default async function Page() {
     return (
         <>
             <div className="container mx-auto py-10">
-                <DataTable columns={columns} data={await fetchPayoutTiers()} pageName="Payout Tiers Page" />
+                <DataTable columns={columns} data={await fetchPayoutTiers()} pageName="Payout Tiers Page" addDialog={<DialogWithButton form="PayoutTierAddForm" title="Add Payout Tier" buttonName="Add Payout Tier +"/>} apiEndpoint="/api/maintenance/payoutTier/payoutTierGet"/>
             </div>
         </>
     );
