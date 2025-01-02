4c894f0a15f7a5c0374dbc6985ee1b80989efd0d
Tyler Dickard <tdickardsoftware@gmail.com>
2025-01-01 22:48:00 -0500

    Define api routes for management

---
+++ leda_project_portal/lib/apiRoutes.ts
@@ -0,0 +1,12 @@
+//
+// Define Route for players api
+//
+export const playerRoute = '/api/management/players';
+//
+// Define Route for places api
+//
+export const placeRoute = '/api/management/places';
+//
+// Define Route for teams api
+//
+export const teamRoute = '/api/management/teams';
