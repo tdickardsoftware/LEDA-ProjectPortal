beeeb8249f54a079a217b529d0a9a70b340ea88b
Tyler Dickard <tdickardsoftware@gmail.com>
2025-01-04 00:30:40 -0500

    Add missing comments in some library scripts

--- leda_project_portal/lib/dbTypeGet.ts
+++ leda_project_portal/lib/dbTypeGet.ts
@@ -3,6 +3,7 @@ import { QueryResult, QueryResultRow } from "pg";
 import { pool } from "./getPool";
 
 export const query = <T extends QueryResultRow>(text: string, params?: any[]): Promise<QueryResult<T>> =>{
+    // Execute the query and return the result casted as the type
     return pool.query<T>(text, params);
 };
 
--- leda_project_portal/lib/query.ts
+++ leda_project_portal/lib/query.ts
@@ -1,20 +1,27 @@
+// Import the database connection pool
 import { pool } from "@/lib/getPool";
 
+// Function to execute a SELECT query
 export async function queryGet(text: string) {
     try {
+        // Execute the query and return the result
         const res = await pool.query(text);
         return res;
     } catch (error) {
+        // Log and rethrow any errors
         console.error("Error executing query:", error);
         throw error;
     }
 }
 
+// Function to execute an INSERT/UPDATE/DELETE query
 export async function queryPost(text: string, values: any) {
     try {
+        // Execute the query with the provided values and return the result
         const res = await pool.query(text, values);
         return res;
     } catch (error) {
+        // Log and rethrow any errors
         console.error("Error executing query:", error);
         throw error;
     }
