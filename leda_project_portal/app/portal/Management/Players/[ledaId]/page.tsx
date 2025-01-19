46790f7b5213a1cd087049af6f8f66dc9cc6500d
Tyler Dickard <tdickardsoftware@gmail.com>
2025-01-19 15:24:59 -0500

    Implement not found page

--- leda_project_portal/app/portal/Management/Players/[ledaId]/page.tsx
+++ leda_project_portal/app/portal/Management/Players/[ledaId]/page.tsx
@@ -1,3 +1,7 @@
+import { notFound } from "next/navigation";
+
 export default function Page({ params }: { params: { ledaId: string } }) {
+
+    notFound();
 	return <p>LEDA ID # {params.ledaId}</p>;
 }
