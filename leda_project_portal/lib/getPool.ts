ec303bde52fc3a3094a93c0b81683f2b6e05509c
Tyler Dickard <tdickardsoftware@gmail.com>
2025-01-20 03:44:04 -0500

    Update Pool

--- leda_project_portal/lib/getPool.ts
+++ leda_project_portal/lib/getPool.ts
@@ -1,5 +1,5 @@
 import { Pool } from "pg";
 // returns the pool
 export const pool = new Pool({
-	connectionString: process.env.POSTGRES_URL,
+	connectionString: process.env.POSTGRES_URL_NO_SSL_LEDA_DB,
 });
