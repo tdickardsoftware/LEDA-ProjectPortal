b0a7769f027db6ce7a44a3f27c1cd4931bf6d356
Tyler Dickard <tdickardsoftware@gmail.com>
2025-01-20 15:19:25 -0500

    Testing

--- leda_project_portal/lib/getPool.ts
+++ leda_project_portal/lib/getPool.ts
@@ -1,13 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-require ("dotenv").config();
+require ("dotenv").config(".env");
 import { Pool } from "pg";
 
 // returns the pool
 export const pool = new Pool({
-	host: process.env.POSTGRES_HOST,
-	database: "leda_db",
-	user: process.env.POSTGRES_USER,
-	password: process.env.POSTGRES_PASSWORD,
-	port: 5432,
-	ssl: true
+	connectionString: process.env.DATABASE_URL,
+	ssl: {
+	  rejectUnauthorized: false,
+	},
 });
