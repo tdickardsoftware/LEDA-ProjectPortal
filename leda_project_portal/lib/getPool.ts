9fbc02c50f6faa888cc42d0ab333ebd15afdc721
Tyler Dickard <tdickardsoftware@gmail.com>
2025-01-20 16:30:01 -0500

    Update connection string to use the correct db

--- leda_project_portal/lib/getPool.ts
+++ leda_project_portal/lib/getPool.ts
@@ -2,9 +2,11 @@
 require ("dotenv").config(".env.preview.local");
 import { Pool } from "pg";
 
+const connectionString = `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.PGHOST}/leda_db?sslmode=require`
+
 // returns the pool
 export const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	connectionString: connectionString,
 	ssl: {
 	  rejectUnauthorized: false,
 	},
