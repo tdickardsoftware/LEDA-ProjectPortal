7a24f5f195293cae6b3eb5dff1d2211577439fad
Tyler Dickard <tdickardsoftware@gmail.com>
2025-01-21 23:03:30 -0500

    Update leda_team_info.sql
    Add member Id list

--- SQL/Tables/leda_team_info.sql
+++ SQL/Tables/leda_team_info.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS public.leda_team_info
     "establishedDate" date NOT NULL,
     "memo" text COLLATE pg_catalog."default",
     "lastTeamFeePayment" text COLLATE pg_catalog."default" NOT NULL,
+    "memberIdList" json NOT NULL,
     CONSTRAINT leda_team_info_pkey PRIMARY KEY (id)
     CONSTRAINT "leda_team_info_ledaId_key" UNIQUE ("ledaId")
 )
