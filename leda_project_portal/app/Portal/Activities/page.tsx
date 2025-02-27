a3305a065de1795a5f95876dcd747280ca08b21e
Tyler Dickard <tdickardsoftware@gmail.com>
2025-02-27 00:19:45 -0500

    Add breadcrumb, fix sidenav issue

--- leda_project_portal/app/Portal/Activities/page.tsx
+++ leda_project_portal/app/Portal/Activities/page.tsx
@@ -3,16 +3,20 @@ import { Separator } from "@/components/ui/separator";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Activities"
+    title: "Activities | LEDA Project Portal"
 }
 
 export default function Page() {
     return (
-        <main>
-			<h1 className="text-4xl font-bold antialiased">Management Page</h1>
-			{/* Separator line */}
-			<Separator className="my-4 bg-gray-500" />
-			<ActivitiesPageContent />
-		</main>
+        <main className="container pl-4">
+            <div className="mb-6 py-2 ">
+                <h1 className="text-4xl font-bold antialiased">Activities</h1>
+                <p className="text-muted-foreground mt-2 mb-4">
+                    View and manage all activities and events related to LEDA projects.
+                </p>
+                <Separator />
+            </div>
+            <ActivitiesPageContent />
+        </main>
     );
 }
