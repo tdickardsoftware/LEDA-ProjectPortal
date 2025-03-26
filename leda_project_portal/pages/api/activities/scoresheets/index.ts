2d5df74bdafc78b39259c131c918dc0042ad292c
Tyler Dickard <tdickardsoftware@gmail.com>
2025-03-26 19:59:43 -0400

    define weekly scoresheet api

---
+++ leda_project_portal/pages/api/activities/scoresheets/index.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { queryPost } from "@/lib/query";
+import { WeeklyScoresheet } from "@/lib/definitions";
+import { query } from "@/lib/dbTypeGet";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === "POST") {
+        const data = req.body as WeeklyScoresheet;
+        try {
+            const query = `
+                INSERT INTO public.leda_weekly_scoresheet ("seasonCode", "weekNumber", "scoresheetData")
+                VALUES ($1, $2, $3)
+                ON CONFLICT ("seasonCode", "weekNumber")
+                DO UPDATE SET "scoresheetData" = $3;
+            `;
+            const values = [data.seasonCode, data.weekNumber, data.scoresheetData];
+            const result = await queryPost(query, values);
+            res.status(201).json(result);
+        } catch (error) {
+            res.status(500).json({ message: "Failed to upsert weekly scoresheet information", error });
+        }
+    } else if (req.method === "GET") {
+        if (req.query.seasonCode) {
+            try {
+                const seasonCode = req.query.seasonCode;
+                const result = await query<WeeklyScoresheet>(`SELECT "seasonCode", "weekNumber", "scoresheetData" FROM public.leda_weekly_scoresheet WHERE "seasonCode" = $1`, [seasonCode as string]);
+                if (result.rows.length !== 0) {
+                    res.status(200).json(result.rows[0]);
+                } else {
+                    res.status(404).json({ message: "No weekly scoresheet information found for the specified season code" });
+                }
+            } catch (error) {
+                res.status(500).json({ message: "Failed to fetch weekly scoresheet information", error });
+            }
+        }
+    } else {
+        res.status(405).json({ message: "Method Not Allowed" });
+    }
+}
