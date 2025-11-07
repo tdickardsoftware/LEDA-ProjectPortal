51a4c3c17e50db99e3b8b53581cb22e2fb663fd3
Tyler Dickard <tdickardsoftware@gmail.com>
2025-11-07 15:48:48 -0500

    Implement GET call for getting teams for a matchup

--- leda_project_portal/pages/api/activities/scoresheets/weeklyScoresheetsV2/teamInfo/index.ts
+++ leda_project_portal/pages/api/activities/scoresheets/weeklyScoresheetsV2/teamInfo/index.ts
@@ -44,5 +44,43 @@ export default async function handler(
         } catch (error) {
             res.status(500).json({ message: "Failed to insert/update team info", error });
         }
+    } else if (req.method === "GET") {
+        if (req.query.seasonCode && req.query.weekNum && req.query.division && req.query.subdivision && req.query.teamLetter) {
+            try {
+                const result = await query<WeeklyScoresheetsTeamInfo>(
+                    `SELECT * FROM public.leda_weekly_scoresheets_team_info WHERE "seasonCode" = $1 AND "weekNum" = $2 AND "division" = $3 AND "subdivision" = $4 AND "teamLetter" = $5`,
+                    [
+                        req.query.seasonCode as string,
+                        req.query.weekNum as string,
+                        req.query.division as string,
+                        req.query.subdivision as string,
+                        req.query.teamLetter as string
+                    ]
+                );
+                const homeResult = result.rows[0]
+                if (homeResult) {
+
+                    const opposingResult = await query<WeeklyScoresheetsTeamInfo>(
+                        `SELECT * FROM public.leda_weekly_scoresheets_team_info WHERE "seasonCode" = $1 AND "weekNum" = $2 AND "division" = $3 AND "subdivision" = $4 AND "teamId" = $5`,
+                        [
+                            req.query.seasonCode as string,
+                            req.query.weekNum as string,
+                            req.query.division as string,
+                            req.query.subdivision as string,
+                            homeResult.opposingTeamId
+                        ]
+                    );
+                    const awayResult = opposingResult.rows[0];
+                    res.status(200).json([homeResult, awayResult]);
+                } else {
+                    res.status(204).json({ message: "No Team Matchup Info Found, Not Created Yet."})
+                }
+            } catch (error) {
+                res.status(500).json({ message: "Failed to fetch team info", error });
+            }
+        }
+
+    } else {
+        res.status(405).json({ error: "Method not allowed" });
     }
 }
