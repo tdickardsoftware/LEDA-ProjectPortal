a44424eeac0c980e59c39c12c2553b43950d07be
Tyler Dickard <tdickardsoftware@gmail.com>
2026-01-14 21:19:37 -0500

    put suspense around useSearchParams()

--- leda_project_portal/app/login/reset-password/page.tsx
+++ leda_project_portal/app/login/reset-password/page.tsx
@@ -1,6 +1,7 @@
 // Import necessary components and types
 import ResetPasswordContent from "@/components/page-content/landing-content/reset-password-content";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 // Define metadata for the page
 export const metadata: Metadata = {
@@ -12,7 +13,9 @@ export default function Page() {
 	return (
 		// Main container for the page content
 		<main>
-            <ResetPasswordContent />
+			<Suspense fallback={<div>Loading...</div>}>
+				<ResetPasswordContent />
+			</Suspense>
 		</main>
 	);
 }
--- leda_project_portal/app/sign-up/page.tsx
+++ leda_project_portal/app/sign-up/page.tsx
@@ -1,6 +1,7 @@
 // Import necessary components and types
 import SignupPageContent from "@/components/page-content/landing-content/signup-page-content";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 // Define metadata for the page
 export const metadata: Metadata = {
@@ -12,7 +13,9 @@ export default function Page() {
 	return (
 		// Main container for the page content
 		<main>
-            <SignupPageContent />
+			<Suspense fallback={<div>Loading...</div>}>
+				<SignupPageContent />
+			</Suspense>
 		</main>
 	);
 }
