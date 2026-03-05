9f2ac7d85e0c4eca5f13f54dd94fca3b860efe90
Tyler Dickard <tdickardsoftware@gmail.com>
2026-03-04 21:40:37 -0500

    remove baseURL in client

--- leda_project_portal/lib/auth-client.ts
+++ leda_project_portal/lib/auth-client.ts
@@ -10,7 +10,6 @@ import { inferAdditionalFields, usernameClient } from "better-auth/client/plugin
 import { createAuthClient } from "better-auth/client"
 
 export const authClient = createAuthClient({
-    baseURL:  window.location.origin || undefined,
     plugins: [
         usernameClient(),
         inferAdditionalFields({
