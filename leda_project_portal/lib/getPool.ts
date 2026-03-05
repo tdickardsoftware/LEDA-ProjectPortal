700f87def54b339c5026515bb334af84c44e8c36
Tyler Dickard <tdickardsoftware@gmail.com>
2026-03-04 22:07:56 -0500

    update url used for pool

--- leda_project_portal/lib/getPool.ts
+++ leda_project_portal/lib/getPool.ts
@@ -11,7 +11,7 @@ import { Pool } from "pg";
 
 // Singleton pool instance shared across all server-side queries
 export const pool = new Pool({
-	connectionString: process.env.POSTGRES_URL,
+	connectionString: process.env.POSTGRES_URL_LEDADB || process.env.POSTGRES_URL,
 	ssl:
 		process.env.POSTGRES_SSL === "true"
 			? { rejectUnauthorized: true }
